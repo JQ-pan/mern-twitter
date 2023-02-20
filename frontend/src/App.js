613b1413b903afcd84fed060055f27acf04b8f1b
Jiong Qi <jiongqipan@gmail.com>
2023-02-19 22:33:15 -0500

    Tweet actions, reducers, routes, profile, and compose tweet components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Switch } from 'react-router-dom';
+
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
 
 import MainPage from './components/MainPage/MainPage';
 import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
+import Tweets from './components/Tweets/Tweets';
+import Profile from './components/Profile/Profile';
+import TweetCompose from './components/Tweets/TweetCompose';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -14,6 +18,7 @@ function App() {
   useEffect(() => {
     dispatch(getCurrentUser()).then(() => setLoaded(true));
   }, [dispatch]);
+
   return loaded && (
     <>
       <NavBar />
@@ -21,6 +26,10 @@ function App() {
         <AuthRoute exact path="/" component={MainPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
+
+        <ProtectedRoute exact path="/tweets" component={Tweets} />
+        <ProtectedRoute exact path="/profile" component={Profile} />
+        <ProtectedRoute exact path="/tweets/new" component={TweetCompose} />
       </Switch>
     </>
   );
